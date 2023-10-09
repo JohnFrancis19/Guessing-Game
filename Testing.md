a2e9f581d139455807cdf148abf34e01da7f2e9a
JohnFrancis19 <sarojohn2018@gmail.com>
2023-10-09 17:18:23 +0800

    For_testing ver.1

---
+++ Testing.md
@@ -0,0 +1,35 @@
+# let targetAge = Math.floor(Math.random() * 100) + 1;
+
+remove cheat codes 
+
+fix upon clicking button in games nothing happen  <p id="result"></p> <!-- This is the line we added -->
+
+added achivements when win a game plus will add $500 to the money as reward so if player want to trya again will have mmore money to buy hints
+
+in this line of codes we add console.log for testing if the code works
+# function handleInvalidInput()
+# function makeGuess()
+  - if (guess === targetAge)
+     - document.getElementById('result').innerText = "Congratulations! You guessed the correct age.";
+     - console.log("Congratulations! You guessed the correct age.");
+     -  else if (guess < targetAge) {
+        document.getElementById('result').innerText = "Too low!";
+        console.log("too low");
+     - } else {
+        document.getElementById('result').innerText = "Too high!";
+        console.log("too high");
+     - }
+      attempts--;
+      updateAttemptsDisplay();
+      console.log("updateAttemptsDisplay() works");
+      } else {
+      handleInvalidInput();
+      console.log("Invalid input. Please enter a valid guess (0-999).");
+     
+# function handleInvalidInput()
+
+# add 1 achievement function 
+    - if player guess the correct answer an achievement will pop up 
+
+    
+
---
+++ script.js
@@ -0,0 +1,164 @@
+// Initialize game variables
+let money = 300;
+let attempts = 5;
+let targetAge = Math.floor(Math.random() * 100) + 1;
+
+// Update money display
+function updateMoneyDisplay() {
+  document.getElementById('money').innerText = money;
+}
+
+// Update attempts display
+function updateAttemptsDisplay() {
+  document.getElementById('attempts').innerText = attempts;
+}
+
+// Handle invalid input
+function handleInvalidInput() {
+  console.log("handleInvalidInput() function was called");
+  document.getElementById('result').innerText = "Invalid input. Please enter a valid guess (0-999).";
+}
+
+// Make a guess
+function makeGuess() {
+  if (attempts > 0 && !document.getElementById('guess').disabled) {
+    const guess = parseInt(document.getElementById('guess').value);
+
+    if (!isNaN(guess) && guess >= 0 && guess <= 999) {
+      if (guess === targetAge) {
+        document.getElementById('result').innerText = "Congratulations! You guessed the correct age.";
+        document.getElementById('play-again-btn').style.display = 'block';
+        document.getElementById('exit-btn').style.display = 'block';
+        showAchievementPopup();  // Display the achievement popup for correct guess
+      } else if (guess < targetAge) {
+        document.getElementById('result').innerText = "Too low!";
+      } else {
+        document.getElementById('result').innerText = "Too high!";
+      }
+      attempts--;
+      updateAttemptsDisplay();
+    } else {
+      handleInvalidInput();
+    }
+
+    if (attempts <= 0) {
+      document.getElementById('play-again-btn').style.display = 'block';
+      document.getElementById('exit-btn').style.display = 'block';
+      document.getElementById('guess').disabled = true;
+    }
+  } else {
+    document.getElementById('result').innerText = "Out of attempts. Game over.";
+    document.getElementById('play-again-btn').style.display = 'block';
+    document.getElementById('exit-btn').style.display = 'block';
+  }
+}
+
+// Buy a hint
+function buyHint(hintNumber) {
+  console.log(`buyHint(${hintNumber}) function was called`);
+  if (attempts > 0 && money > 0) {
+    let hintPrice = 0;
+    let hintDescription = '';
+
+    switch (hintNumber) {
+      case 1:
+        hintPrice = 300;
+        hintDescription = "Hint 1: The age is within 10 years of " + targetAge;
+        break;
+      case 2:
+        hintPrice = 500;
+        hintDescription = "Hint 2: The age is within 5 years of " + targetAge;
+        break;
+      case 3:
+        hintPrice = 1000;
+        hintDescription = "Hint 3: The age is within 1 year of " + targetAge;
+        break;
+      default:
+        break;
+    }
+
+    if (money >= hintPrice) {
+      document.getElementById('result').innerText = hintDescription;
+      money -= hintPrice;
+      updateMoneyDisplay();
+      return;
+    }
+  }
+
+  document.getElementById('result').innerText = "Insufficient funds to buy a hint.";
+}
+
+// Reset attempts
+function resetAttempts() {
+  attempts = 5;
+  updateAttemptsDisplay();
+}
+
+// Play again
+function playAgain() {
+  money = 300;
+  attempts = 5;
+  targetAge = Math.floor(Math.random() * 100) + 1;
+  document.getElementById('cheat-indicator').innerText = "";
+  updateMoneyDisplay();
+  updateAttemptsDisplay();
+  document.getElementById('result').innerText = "";
+  document.getElementById('play-again-btn').style.display = 'none';
+  document.getElementById('exit-btn').style.display = 'none';
+  document.getElementById('guess').value = "";
+  document.getElementById('guess').disabled = false;
+}
+
+// Show customization options
+function showCustomizationOptions() {
+  console.log('showCustomizationOptions() called');
+  const gameContainer = document.getElementById('game-container');
+  gameContainer.style.backgroundColor = prompt('Enter background color (e.g., red, #00FF00):');
+}
+
+function showPlayOptions() {
+  console.log('showPlayOptions() called');
+  const titleScreen = document.getElementById('title-screen');
+  const gameContainer = document.getElementById('game-container');
+  const buttonsContainer = document.getElementById('buttons-container');
+
+  // Hide the title-screen
+  titleScreen.style.display = 'none';
+
+  // Show the game container and buttons-container
+  gameContainer.style.display = 'block';
+  buttonsContainer.style.display = 'block';
+
+  document.getElementById('result').innerText = "Game started!";
+}
+
+
+
+// Show exit prompt
+function showExitPrompt() {
+  console.log('showExitPrompt() called');
+  const exitConfirmation = confirm("Are you sure you want to exit the game?\n\nDeveloped by [Your Name or Team]");
+  if (exitConfirmation) {
+    exitGame();
+  }
+}
+
+// Exit game
+function exitGame() {
+  alert("Thank you for playing EraGlimpse: Age Unveiled!\nDeveloped by [JOHN FRANCIS SARO]");
+}
+
+// Show achievement popup
+function showAchievementPopup() {
+  const popup = document.getElementById('achievement-popup');
+  popup.style.display = 'block';
+
+  // Hide the popup after a few seconds (e.g., 3 seconds)
+  setTimeout(() => {
+    popup.style.display = 'none';
+  }, 3000);
+}
+
+// Call necessary functions
+updateMoneyDisplay();
+updateAttemptsDisplay();
